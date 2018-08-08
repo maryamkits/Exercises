c1aac7e3faeafe41fae466697ad1ea6cfaa2d340
maryamkits <maryam.kits@gmail.com>
2018-08-08 15:45:52 +0400

    Add files via upload

---
+++ 25July/delay.js
@@ -0,0 +1,8 @@
+function delay(ms) { 
+    return new Promise(function(resolve){
+        setTimeout(() =>{ 
+            resolve(ms)
+            }, ms);
+    })    
+}
+delay(3000).then((ms)=>alert(`runs after ${ms/1000} seconds`));
