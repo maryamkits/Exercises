cbdcb981734ba194c185f6a544967b89ce5406d9
Maryam Kits <MaryamKits@Givis-MacBook-Pro.local>
2018-10-29 12:18:18 +0400

    mini-project

--- Project/mini-project/server.js
+++ Project/mini-project/server.js
@@ -26,12 +26,26 @@ app.get('/', (req, res) => {
 })
 app.post('/', (req, res) => {
     const {person_name, lastname, fathersname, id, birthday} = req.body
-    people.push({person_name, lastname, fathersname, id, birthday})
+    people.push({person_name, lastname, fathersname, id, birthday, cars})
     res.render('index', {
                 title: 'Users',
                 peoplePage: true,
                 people,
             })
+})
+app.post('/userSearch', (req, res) => {
+    const id = req.body.personSearch.split('id: ')[1]
+    res.redirect(`/userSearch/${id}`)
+
+})
+app.get('/userSearch/:id', (req, res) => {
+    const foundPerson = people.find(person => person.id == req.params.id)
+    res.render('index', {
+        title: 'Person Info',
+        showPerson: true,
+        cars,
+        foundPerson
+    })
 })
 app.get('/editPeople/:id', (req, res) => {
     let id = req.params.id
@@ -39,7 +53,6 @@ app.get('/editPeople/:id', (req, res) => {
     res.render('index', {
                 title: 'Edit User',
                 editPeople: true,
-                // people,
                 person
     })
 })
@@ -49,11 +62,6 @@ app.post('/editPeople/:id', (req, res) => {
     const {person_name, lastname, fathersname, id, birthday} = req.body
     let i = people.indexOf(person.id)
     people.splice(i, 1, {person_name, lastname, fathersname, id, birthday})
-    // res.render('index', {
-    //             title: 'Edit User',
-    //             editPeople: true,
-    //             people,
-    // })
     res.redirect(`/`)
 })
 
@@ -67,12 +75,29 @@ app.get('/cars', (req, res) => {
 })
 app.post('/cars', (req, res) => {
     const {owner, car_name, model, vin, num, color} = req.body
-    cars.push({owner, car_name, model, vin, num, color})
+    // cars.push({owner, car_name, model, vin, num, color})
+    const person = owner.split(' ')
+    const user = people.find(user => user.person_name == person[0] && user.lastname == person[1])
+    user.cars.push({owner, car_name, model, vin, num, color})
+    console.log
     res.render('index', {
                 title: 'Cars',
                 carPage: true,
                 people,
                 cars
+    })
+})
+app.post('/carSearch', (req, res) => {
+    const vin = req.body.carSearch.split('VIN:')[1]
+    res.redirect(`/carSearch/${vin}`)
+})
+app.get('/carSearch/:vin', (req, res) => {
+    const foundCar = cars.find(car => car.vin == req.params.vin)
+    res.render('index', {
+        title: 'Car Info',
+        showCar: true,
+        cars,
+        foundCar
     })
 })
 app.get('/editCar/:vin', (req, res) => {
