302523774881d8645974f749a85cb3fda83c1e83
Maryam Kits <MaryamKits@Givis-MacBook-Pro.local>
2018-11-13 15:33:52 +0400

    slider

---
+++ November11/exercise/app.js
@@ -0,0 +1,12 @@
+const arrow = document.querySelector('.arrow')
+const imagesList = document.querySelectorAll('.slider')
+console.log(imagesList.length)
+
+for(let i = 0; i < 5; i++){
+    imagesList[i].classList.add("shown");
+}
+arrow.addEventListener('click', function() {
+    for(let i = 0; i < 5; i++){
+        imagesList[i].classList.add("hidden");
+    }
+})
