ae807c929a3d7094d28eff663bac571f5a879c9a
Maryam Kits <MaryamKits@Givis-MacBook-Pro.local>
2018-12-05 12:56:41 +0400

    build

---
+++ November16/app/build/precache-manifest.b4fb4346a4b742b7c2fb7de915c8f6c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ad684c9536814b4340ef",
+    "url": "/static/js/main.ad684c95.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "ad684c9536814b4340ef",
+    "url": "/static/css/main.70036518.chunk.css"
+  },
+  {
+    "revision": "2d4f90287db91c8b2a9183759b230e35",
+    "url": "/index.html"
+  }
+];
